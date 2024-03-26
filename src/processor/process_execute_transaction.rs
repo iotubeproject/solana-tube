67236036054b90665466531ca3cfdec26b6a6470
Liuhaai <haixiang@iotex.io>
2024-03-25 22:00:32 -0700

    save

--- src/processor/process_execute_transaction.rs
+++ src/processor/process_execute_transaction.rs
@@ -24,30 +24,32 @@ pub fn process_execute_transaction(program_id: &Pubkey, accounts: &[AccountInfo]
     let account_info_iter = &mut accounts.iter();
 
     let governance_info = next_account_info(account_info_iter)?; // 0
-    let proposal_info = next_account_info(account_info_iter)?; // 1
-    let proposal_transaction_info = next_account_info(account_info_iter)?; // 2
+
+    // let proposal_info = next_account_info(account_info_iter)?; // 1
+    // let proposal_transaction_info = next_account_info(account_info_iter)?; // 2
 
     let clock = Clock::get()?;
 
     let governance_data = get_governance_data(program_id, governance_info)?;
 
-    let mut proposal_data =
-        get_proposal_data_for_governance(program_id, proposal_info, governance_info.key)?;
+    // let mut proposal_transaction_data = get_proposal_transaction_data_for_proposal(
+    //     program_id,
+    //     proposal_transaction_info,
+    //     proposal_info.key,
+    // )?;
 
-    let mut proposal_transaction_data = get_proposal_transaction_data_for_proposal(
-        program_id,
-        proposal_transaction_info,
-        proposal_info.key,
-    )?;
-
-    proposal_data
-        .assert_can_execute_transaction(&proposal_transaction_data, clock.unix_timestamp)?;
+    // Proposal is always executed in the POC
+    // TODO: Enable Proposal after POC
+    // let mut proposal_data =
+    //     get_proposal_data_for_governance(program_id, proposal_info, governance_info.key)?;
+    // proposal_data
+    //     .assert_can_execute_transaction(&proposal_transaction_data, clock.unix_timestamp)?;
 
     // Execute instruction with Governance PDA as signer
-    let instructions = proposal_transaction_data
-        .instructions
-        .iter()
-        .map(Instruction::from);
+    // let instructions = proposal_transaction_data
+    //     .instructions
+    //     .iter()
+    //     .map(Instruction::from);
 
     // In the current implementation accounts for all instructions are passed to
     // each instruction invocation. This is an overhead but shouldn't be a
@@ -67,55 +69,27 @@ pub fn process_execute_transaction(program_id: &Pubkey, accounts: &[AccountInfo]
 
     signers_seeds.push(&governance_seeds[..]);
 
-    // Sign the transaction using the governance treasury PDA if required by the
-    // instruction
-    let mut treasury_seeds = get_native_treasury_address_seeds(governance_info.key).to_vec();
-    let (treasury_address, treasury_bump_seed) =
-        Pubkey::find_program_address(&treasury_seeds, program_id);
-    let treasury_bump = &[treasury_bump_seed];
+    // TODO: Enable Proposal after POC
+    // proposal_data.executing_at = Some(clock.unix_timestamp);
+    // proposal_data.state = ProposalState::Executing;
 
-    if instruction_account_infos
-        .iter()
-        .any(|a| a.key == &treasury_address)
-    {
-        treasury_seeds.push(treasury_bump);
-        signers_seeds.push(&treasury_seeds[..]);
-    }
-
+    // TODO: Hardcode Mint_to instruction for POC
     for instruction in instructions {
         invoke_signed(&instruction, instruction_account_infos, &signers_seeds[..])?;
     }
 
     // Update proposal and instruction accounts
-    if proposal_data.state == ProposalState::Succeeded {
-        proposal_data.executing_at = Some(clock.unix_timestamp);
-        proposal_data.state = ProposalState::Executing;
-    }
 
-    let option = &mut proposal_data.options[proposal_transaction_data.option_index as usize];
-    option.transactions_executed_count = option.transactions_executed_count.checked_add(1).unwrap();
+    // TODO: Enable Proposal after POC
+    // let option = &mut proposal_data.options[proposal_transaction_data.option_index as usize];
+    // option.transactions_executed_count = option.transactions_executed_count.checked_add(1).unwrap();
+    // proposal_data.closed_at = Some(clock.unix_timestamp);
+    // proposal_data.state = ProposalState::Completed;
+    // proposal_data.serialize(&mut proposal_info.data.borrow_mut()[..])?;
 
-    // Checking for Executing and ExecutingWithErrors states because instruction can
-    // still be executed after being flagged with error The check for
-    // instructions_executed_count ensures Proposal can't be transitioned to
-    // Completed state from ExecutingWithErrors
-    if (proposal_data.state == ProposalState::Executing
-        || proposal_data.state == ProposalState::ExecutingWithErrors)
-        && proposal_data
-            .options
-            .iter()
-            .filter(|o| o.vote_result == OptionVoteResult::Succeeded)
-            .all(|o| o.transactions_executed_count == o.transactions_count)
-    {
-        proposal_data.closed_at = Some(clock.unix_timestamp);
-        proposal_data.state = ProposalState::Completed;
-    }
-
-    proposal_data.serialize(&mut proposal_info.data.borrow_mut()[..])?;
-
-    proposal_transaction_data.executed_at = Some(clock.unix_timestamp);
-    proposal_transaction_data.execution_status = TransactionExecutionStatus::Success;
-    proposal_transaction_data.serialize(&mut proposal_transaction_info.data.borrow_mut()[..])?;
+    // proposal_transaction_data.executed_at = Some(clock.unix_timestamp);
+    // proposal_transaction_data.execution_status = TransactionExecutionStatus::Success;
+    // proposal_transaction_data.serialize(&mut proposal_transaction_info.data.borrow_mut()[..])?;
 
     Ok(())
 }
